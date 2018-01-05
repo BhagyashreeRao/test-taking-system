06a86c276ab72c339657cb1a48a6185bb262a009
BhagyashreeRao <kbhagyashree.rao@gmail.com>
2018-01-05 21:50:08 +0530

    fix

--- libs/sendMail.js
+++ libs/sendMail.js
@@ -3,23 +3,23 @@ exports.send = function(from,to,subject,text,html){
 	var smtpTransport = require('nodemailer-smtp-transport');
 	
 	var transporter = nodemailer.createTransport(smtpTransport({
-	   //First enter Service of your email
+	   //First enter Service of your email
 	   service: 'Gmail',
-	   auth: {
+	   auth: {
 		   //Enter email here
-	       user: 'your gmail email',
+	       user: 'your gmail email',
 
 		   //Enter your password here
-	       pass: 'your gmail password'
-	   }
+	       pass: 'your gmail password'
+	   }
 	}));
     //Send mail
 	transporter.sendMail({
-           from: from,
-           to: to,
-           subject: subject,
-           text: text,
-           html: html
+           from: from,
+           to: to,
+           subject: subject,
+           text: text,
+           html: html
     });
 	//Free up the resources
     transporter.close();     
